c2b40c7399c72f2393a56beb753e7bf27796bac6
vycanismajoris1001-gif <vycanismajoris1001@gmail.com>
2026-05-15 09:42:08 +0900

    Update 3-2.c

---
+++ code/3-2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int cmp_asc(const void* a, const void* b) {
+  return *(const void*)a - *(const void*)b;
+}
+
+int cmp_desc(const void* a, const void* b) {
+  return *(const void*)b - *(const void*)a;
+}
+
+void print_arr(int* arr, int size) {
+  for(int i = 0; i < size ; i++) {
+     printf("%d ", arr[i]);
+  }
+  printf("\n");
+}
+
+int main(void) {
+  int arr[] - {5, 2, 8, 1, 9, 3};
+  int size = 6;
+
+  qsort(arr, 6, sizeof(int), cmp_asc);
+  print_arr(arr, size);
+
+  qsort(arr, 6, sizeof(int), cmp_desc);
+  print_arr(arr, size);
+
+  return 0;
+}
